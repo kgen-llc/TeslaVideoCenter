5c923ebb616b943383fcb4dbd16c3573364ede71
Frederic Forjan <fforjan@free.fr>
2020-11-27 16:30:23 -0800

    Add VideoView control

--- TeslaVideoCenter/Views/MainWindow.xaml.cs
+++ TeslaVideoCenter/Views/MainWindow.xaml.cs
@@ -2,57 +2,19 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
-using LibVLCSharp.Avalonia;
-using LibVLCSharp.Shared;
-using TeslaVideoCenter.ViewModels;
 
 namespace TeslaVideoCenter.Views
 {
     public class MainWindow : Window
     {
-        private VideoView VideoView;
-        private LibVLC _libVLC;
-        private MediaPlayer _mediaPlayer;
-
         public MainWindow()
         {
             InitializeComponent();
-
-             VideoView = this.Get<VideoView>("VideoView");
-
-            _libVLC = new LibVLC();
-            _mediaPlayer = new MediaPlayer(_libVLC);
-
-            VideoView.MediaPlayer = _mediaPlayer;
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
-        }
-
-
-
-        private void StopButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (VideoView.MediaPlayer.IsPlaying)
-            {
-                VideoView.MediaPlayer.Stop();
-            }
-        }
-
-        private void PlayButton_Click(object sender, RoutedEventArgs e)
-        {
-            var video = ((MainWindowViewModel)this.DataContext).CurrentVideo;
-            if (!VideoView.MediaPlayer.IsPlaying && video != null)
-            {
-                VideoView.MediaPlayer.Play(new Media(_libVLC,video.FilePath, FromType.FromPath));
-            }
-        }
-
-        private void PauseButton_Click(object sender, RoutedEventArgs e)
-        {
-            VideoView.MediaPlayer.Pause();
         }
     }
 }
---
+++ TeslaVideoCenter/Views/VideoView.xaml.cs
@@ -0,0 +1,57 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using LibVLCSharp.Avalonia;
+using LibVLCSharp.Shared;
+using TeslaVideoCenter.ViewModels;
+
+namespace TeslaVideoCenter.Views
+{
+    public class VideoView : UserControl
+    {
+        private LibVLCSharp.Avalonia.VideoView VlcVideoView;
+        private LibVLC _libVLC;
+        private MediaPlayer _mediaPlayer;
+
+
+        public VideoView()
+        {
+            InitializeComponent();
+
+              VlcVideoView = this.Get<LibVLCSharp.Avalonia.VideoView>("VideoView");
+
+            _libVLC = new LibVLC();
+            _mediaPlayer = new MediaPlayer(_libVLC);
+
+            VlcVideoView.MediaPlayer = _mediaPlayer;
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+           private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (VlcVideoView.MediaPlayer.IsPlaying)
+            {
+                VlcVideoView.MediaPlayer.Stop();
+            }
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            var video = this.DataContext as string;
+            if (!VlcVideoView.MediaPlayer.IsPlaying && video != null)
+            {
+                VlcVideoView.MediaPlayer.Play(new Media(_libVLC,video, FromType.FromPath));
+            }
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            VlcVideoView.MediaPlayer.Pause();
+        }
+    }
+}
