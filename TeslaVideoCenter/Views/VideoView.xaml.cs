5165e100b46a14fc433e2062d9729fdfd2f12bca
Frederic Forjan <fforjan@free.fr>
2020-11-28 16:13:21 -0800

    some cleanup

--- TeslaVideoCenter/Views/VideoView.xaml.cs
+++ TeslaVideoCenter/Views/VideoView.xaml.cs
@@ -1,15 +1,11 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Avalonia.Threading;
-using LibVLCSharp.Avalonia;
 using LibVLCSharp.Shared;
-using TeslaVideoCenter.ViewModels;
 
 namespace TeslaVideoCenter.Views
 {
@@ -31,14 +27,15 @@ namespace TeslaVideoCenter.Views
             InitializeComponent();
 
             VlcVideoView = this.Get<LibVLCSharp.Avalonia.VideoView>("VideoView");
-            CurrentPart  = this.Get<Avalonia.Controls.TextBlock>("CurrentPart");
+            CurrentPart = this.Get<Avalonia.Controls.TextBlock>("CurrentPart");
             _libVLC = new LibVLC();
             _mediaPlayer = new MediaPlayer(_libVLC);
 
-            this.CurrentPart.Text = "-1";
+            this.CurrentPart.Text = "No Video Selected";
 
             VlcVideoView.MediaPlayer = _mediaPlayer;
-            _mediaPlayer.EndReached +=  (sender, arg) => {
+            _mediaPlayer.EndReached += (sender, arg) =>
+            {
                 PlayCurrent();
             };
         }
@@ -48,7 +45,7 @@ namespace TeslaVideoCenter.Views
             AvaloniaXamlLoader.Load(this);
         }
 
-           private void StopButton_Click(object sender, RoutedEventArgs e)
+        private void StopButton_Click(object sender, RoutedEventArgs e)
         {
             if (VlcVideoView.MediaPlayer.IsPlaying)
             {
@@ -59,31 +56,43 @@ namespace TeslaVideoCenter.Views
         private void PlayButton_Click(object sender, RoutedEventArgs e)
         {
             var video = this.DataContext as string[];
-            this.currentVideo = video.Cast<string>().GetEnumerator();
-             this.currentPart = 0;
-            if (!VlcVideoView.MediaPlayer.IsPlaying && this.currentVideo != null)
+            if (video == null)
             {
-               PlayCurrent();
+                this.CurrentPart.Text = "No Video Selected";
+            }
+            else
+            {
+                this.currentVideo = video.Cast<string>().GetEnumerator();
+                this.currentPart = 0;
+                if (!VlcVideoView.MediaPlayer.IsPlaying && this.currentVideo != null)
+                {
+                    PlayCurrent();
+                }
             }
         }
 
         private void PlayCurrent()
         {
             // ensure we are on the UI thread for changing this
-            if(Dispatcher.UIThread.CheckAccess()) { 
-           
-                if(this.currentVideo.MoveNext()) {
-                        this.currentPart++;
-                        VlcVideoView.MediaPlayer.Play(new Media(_libVLC,this.currentVideo.Current, FromType.FromPath));
-                }else {
+            if (Dispatcher.UIThread.CheckAccess())
+            {
+
+                if (this.currentVideo.MoveNext())
+                {
+                    this.currentPart++;
+                    VlcVideoView.MediaPlayer.Play(new Media(_libVLC, this.currentVideo.Current, FromType.FromPath));
+                }
+                else
+                {
                     this.currentPart = -1;
                 }
 
-                this.CurrentPart.Text = "Part : "+ this.currentPart.ToString();
+                this.CurrentPart.Text = "Part : " + this.currentPart.ToString();
             }
-             else {
-                 Dispatcher.UIThread.Post(new Action(this.PlayCurrent));
-             }
+            else
+            {
+                Dispatcher.UIThread.Post(new Action(this.PlayCurrent));
+            }
         }
 
         private void PauseButton_Click(object sender, RoutedEventArgs e)
