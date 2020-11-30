3a54f030873eee09fdf08d97c657ae97ada10d2b
Frederic Forjan <fforjan@free.fr>
2020-11-29 17:09:22 -0800

    add cmmand hanlder

--- TeslaVideoCenter.Common/ViewModels/EventViewModel.cs
+++ TeslaVideoCenter.Common/ViewModels/EventViewModel.cs
@@ -1,3 +1,6 @@
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ReactiveUI;
 using TeslaVideoCenter.Models;
 
 namespace TeslaVideoCenter.ViewModels
@@ -6,10 +9,17 @@ namespace TeslaVideoCenter.ViewModels
     {
         public EventViewModel(Event @event) {
             this.Event = @event;
+
+            this.GenerateOverallVideoCommand = ReactiveCommand.Create(GenerateOverallVideo);
         }
 
-
         public Event Event {get;}
+
+        public ICommand GenerateOverallVideoCommand {get;}
+
+        private Task GenerateOverallVideo() {
+            return Task.CompletedTask;
+        }
     }
 
 }
