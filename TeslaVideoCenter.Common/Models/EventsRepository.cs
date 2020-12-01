c34711d0093fd7205fe2d25b1e502900fd4c019b
Frederic Forjan <fforjan@free.fr>
2020-12-01 12:00:18 -0800

    create directory if it doesn't exists

--- TeslaVideoCenter.Common/Models/EventsRepository.cs
+++ TeslaVideoCenter.Common/Models/EventsRepository.cs
@@ -7,6 +7,12 @@ namespace TeslaVideoCenter.Models
     public class EventsRepository  {
 
         public EventsRepository(string folder) {
+
+            // ensure the directory exists
+            if(!Directory.Exists(folder)) {
+                Directory.CreateDirectory(folder);
+            }
+
             this.Events = new ObservableCollectionExtended<Event>(
                 Directory
                     .GetFiles(folder, "event.json", SearchOption.AllDirectories)
